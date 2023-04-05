da9a72e44d9fe984c2da8923b81f8248a8a2124e
bhavana <bhavana.bhangale@icreativetechnolabs.com>
2023-04-05 18:58:10 +0530

    Entity Defination 05/04/2023

---
+++ custom/plugins/SwagShopFinder/src/Core/Content/ShopFinder/ShopFinderEntity.php
@@ -0,0 +1,84 @@
+<?php declare(strict_types=1);
+
+namespace SwagShopFinder\Core\Content\ShopFinder;
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+use Shopware\Core\System\Country\CountryEntity;
+
+class ShopFinderEntity extends Entity
+{
+    use EntityIdTrait;
+    protected bool $active;
+    protected ?string $name;
+    protected ?string $description;
+    protected ?string $city;
+    protected ?string $telephone;
+    protected ?CountryEntity $country;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+    public function setId(?string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): void
+    {
+        $this->city = $city;
+    }
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): void
+    {
+        $this->telephone = $telephone;
+    }
+
+    public function getCountry(): ?CountryEntity
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?CountryEntity $country): void
+    {
+        $this->country = $country;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): void
+    {
+        $this->active = $active;
+    }
+}
---
+++ custom/plugins/SwagShopFinder/src/Core/api/DataDemoController.php
@@ -0,0 +1,8 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DataDemoController extends AbstractController
+{
+
+}
