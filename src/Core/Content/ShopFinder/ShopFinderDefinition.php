0f1f15b25f9631a6333084be490a86f867d9d06c
bhavana <bhavana.bhangale@icreativetechnolabs.com>
2023-04-05 11:29:02 +0530

    add file

---
+++ src/Core/Content/ShopFinder/ShopFinderDefinition.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace SwagShopFinder\Core\Content\Example;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+
+class ExampleDefinition extends EntityDefinition
+{
+    public const ENTITY_NAME = 'swag_example'; //database table name
+
+    public function getEntityName(): string
+    {
+        return self::ENTITY_NAME;   //give entity class name
+    }
+
+    protected function defineFields(): FieldCollection
+    {
+        return new FieldCollection([]);   //define database field
+    }
+}
